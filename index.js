f9cfbd9224e1d4881366d203a565ffe59078f75d
SemihDurmus <semihdurmus@yahoo.com>
2020-09-21 23:38:37 +0200

    Bug : Bug fixes

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ function nr_checker() {
 
             document.getElementById("reset").style.display = "inline-block";
             document.getElementById("check").style.display = "none";
-            document.getElementById("reset").style.display = "inline-block";
 
             if (counter <= 3) {
                 document.getElementById("directions").innerHTML = "Congrats!! You found the number in " + counter + ". attepmt!"
@@ -30,10 +29,12 @@ function nr_checker() {
             }
         } else if (guess < random_nr) {
             document.getElementById("directions").innerHTML = "You should go up!";
+            document.getElementById("inp").value = "";
             counter++;
         } else {
             document.getElementById("directions").innerHTML = "You should go down!";
             counter++;
+            document.getElementById("inp").value = "";
         }
     } else {
         alert("This is not a number")
